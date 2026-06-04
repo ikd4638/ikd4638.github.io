fe35b023e6b98b715d74f750db9bd606e75a401b
Indu Kalap Dihingia <ikd4638@gmail.com>
2026-06-05 07:11:04 +0800

    Update publications renderer 1

--- js/publications.js
+++ js/publications.js
@@ -1,67 +1,22 @@
-let papers = [];
+html += `
+<div class="card mb-3">
+  <div class="card-body">
 
-fetch("data/publications.json")
-.then(response => response.json())
-.then(data => {
-    papers = data;
-    render(papers);
-})
-.catch(error => {
-    console.error("Could not load publications.json", error);
-});
+    <h5>
+      <a href="${p.url}" target="_blank">
+        ${p.title}
+      </a>
+    </h5>
 
-function render(list) {
+    <p>${p.display_authors}</p>
 
-    let html = "";
+    <p>
+      ${p.journal},
+      ${p.volume},
+      ${p.pages}
+      (${p.year})
+    </p>
 
-    list.forEach(p => {
-
-        html += `
-        <div class="card mb-3">
-            <div class="card-body">
-
-                <h5>
-                    <a href="${p.url || '#'}" target="_blank">
-                        ${p.title}
-                    </a>
-                </h5>
-
-                <p>
-                    ${p.display_authors || ""}
-                </p>
-
-                <p>
-                    ${p.journal || ""}
-                    ${p.volume ? ", Vol. " + p.volume : ""}
-                    ${p.pages ? ", " + p.pages : ""}
-                    ${p.year ? " (" + p.year + ")" : ""}
-                </p>
-
-            </div>
-        </div>
-        `;
-
-    });
-
-    document.getElementById("papers").innerHTML = html;
-}
-
-function showAll() {
-    render(papers);
-}
-
-function showFirstAuthor() {
-    render(
-        papers.filter(
-            p => p.first_author
-        )
-    );
-}
-
-function showManyAuthors() {
-    render(
-        papers.filter(
-            p => p.many_authors
-        )
-    );
-}
+  </div>
+</div>
+`;
