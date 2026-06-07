cceeba529334da3179f6540ea1ba4b0f1da17564
Indu Kalap Dihingia <ikd4638@gmail.com>
2026-06-07 09:03:36 +0800

    Remove markdown fences

--- js/publications.js
+++ js/publications.js
@@ -1,22 +1,96 @@
-html += `
-<div class="card mb-3">
-  <div class="card-body">
+let papers = [];
 
-    <h5>
-      <a href="${p.url}" target="_blank">
-        ${p.title}
-      </a>
-    </h5>
+fetch("data/publications.json")
+.then(response => response.json())
+.then(data => {
 
-    <p>${p.display_authors}</p>
+papers = data;
 
-    <p>
-      ${p.journal},
-      ${p.volume},
-      ${p.pages}
-      (${p.year})
-    </p>
+showAll();
 
-  </div>
-</div>
-`;
+})
+.catch(error => {
+
+console.error(
+    "Could not load publications.json",
+    error
+);
+
+});
+
+function render(list) {
+
+const container =
+    document.getElementById("papers");
+
+let html = "";
+
+list.forEach(p => {
+
+    html += `
+    <div class="card mb-3">
+        <div class="card-body">
+
+            <h5>
+                <a href="${p.url}"
+                   target="_blank">
+
+                    ${p.title}
+
+                </a>
+            </h5>
+
+            <p>
+                ${p.display_authors}
+            </p>
+
+            <p>
+
+                ${p.journal}
+
+                ${p.volume ?
+                ", Vol. " + p.volume : ""}
+
+                ${p.pages ?
+                ", " + p.pages : ""}
+
+                (${p.year})
+
+            </p>
+
+        </div>
+    </div>
+    `;
+
+});
+
+container.innerHTML = html;
+
+}
+
+function showAll() {
+
+render(papers);
+
+}
+
+function showFirstAuthor() {
+
+render(
+    papers.filter(
+        p => p.first_author
+    )
+);
+
+}
+
+function showManyAuthors() {
+
+render(
+    papers.filter(
+        p => p.many_authors
+    )
+);
+
+}
+
