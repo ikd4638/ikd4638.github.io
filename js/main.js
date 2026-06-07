c5add55edab2fbe06b828303bc35a371fb884b1e
Indu Kalap Dihingia <ikd4638@gmail.com>
2026-06-07 18:41:44 +0800

    auto update

--- js/main.js
+++ js/main.js
@@ -3,13 +3,25 @@ fetch("data/metrics.json")
 .then(data => {
 
     document.getElementById("pubs").textContent =
-        data.total_publications;
+    data.total_publications;
 
     document.getElementById("firstauthor").textContent =
-        data.first_author;
+    data.first_author_publications;
 
     document.getElementById("manyauthors").textContent =
-        data.many_authors;
+    data.papers_with_many_authors;
+
+    document.getElementById("citations").textContent =
+    data.total_citations;
+
+    document.getElementById("hindex").textContent =
+    data.h_index;
+
+    document.getElementById("i10index").textContent =
+    data.i10_index;
+
+    document.getElementById("lastupdated").textContent =
+    new Date(data.last_updated).toLocaleDateString();
 })
 .catch(error => {
     console.log("Could not load metrics:", error);
